ed2ff49ca04b60619f16e6c23c904f385f17d7bd
Marshall Derksen <theredmugproductions@gmail.com>
2026-05-06 11:28:01 -0700

    Fixed logic to use correct recombobulator price

--- data_sources/hypixel/fetchItems.js
+++ data_sources/hypixel/fetchItems.js
@@ -2,9 +2,12 @@ import { existsSync } from "node:fs";
 import { readJsonFile, writeJsonFile, writeTsvFile } from "../ioUtil.js";
 
 const HYPIXEL_ITEMS_URL = "https://api.hypixel.net/v2/resources/skyblock/items";
+const HYPIXEL_PRICES_URL = "https://api.hypixel.net/v2/skyblock/bazaar";
 
 const ITEMS_JSON_PATH = "data/items.json";
 const ITEMS_TABLE_PATH = "data/items.tsv";
+const PRICES_JSON_PATH = "data/prices.json";
+const PRICES_TABLE_PATH = "data/prices.tsv";
 
 function parseCmdArgs() {
     const result = {};
@@ -37,14 +40,41 @@ async function fetchItems() {
     return result;
 }
 
+async function fetchPrices() {
+    const result = {
+        timeStamp: new Date().toISOString(),
+        prices: []
+    };
+
+    const pricesRes = await fetch(HYPIXEL_PRICES_URL);
+    const pricesObject = await pricesRes.json();
+
+    if (!pricesObject.success) {
+        console.error("Fetch official prices usuccessful");
+        return;
+    }
+
+    for (const id in pricesObject.products) {
+        result.prices.push(pricesObject.products[id].quick_status);
+    }
+
+    return result;
+}
+
 const args = parseCmdArgs();
 
 let itemsObject;
+let pricesObject;
 if ("fetchAll" in args || !existsSync(ITEMS_JSON_PATH)) {
     itemsObject = await fetchItems();
     await writeJsonFile(itemsObject, ITEMS_JSON_PATH);
+
+    pricesObject = await fetchPrices();
+    await writeJsonFile(pricesObject, PRICES_JSON_PATH);
 } else {
     itemsObject = await readJsonFile(ITEMS_JSON_PATH);
+    pricesObject = await readJsonFile(PRICES_JSON_PATH);
 }
 
 writeTsvFile(itemsObject.items, ITEMS_TABLE_PATH);
+writeTsvFile(pricesObject.prices, PRICES_TABLE_PATH);
